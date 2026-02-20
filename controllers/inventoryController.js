67518d794abb7d6ad5bb17a04ee24b995ad6ca13
vinodvinnueswar <vinodvinnu0899@gmail.com>
2026-02-21 01:40:51 +0530

    Clean

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -19,9 +19,16 @@ cloudinary.config({
    ✅ Multer Storage Setup
 ================================= */
 const storage = multer.diskStorage({
- destination: function (req, file, cb) {
-  cb(null, path.join(__dirname, "../uploads"));
-},
+  destination: function (req, file, cb) {
+    const uploadPath = path.join(__dirname, "../uploads");
+
+    // ✅ Create folder if it doesn't exist
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath, { recursive: true });
+    }
+
+    cb(null, uploadPath);
+  },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
   },
